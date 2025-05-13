9bd402c24f2f1cdb35135d308fbe340eeb531f82
Oliver <olialexander08@gmail.com>
2025-05-13 09:35:10 +0300

    fiix program

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -5,7 +5,7 @@ const Schedule = () => {
     return (
         <div className={style.schedule}>
             <h2><FaClock className={style.icon} /> Program</h2>
-            <p><strong>Luni – Vineri:</strong> 9:00 – 17:00</p>
+            <p><strong>Luni – Vineri:</strong> 8:00 – 16:00</p>
             <p><strong>Sambata – Duminica:</strong> Inchis</p>
         </div>
     );
